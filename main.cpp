3eaad5e869760557202d3855b3e463e6b9bf2133
mkuczkowski <incogniotoo@gmail.com>
2019-04-12 15:53:18 +0200

    Basic one column collision added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 float playerPosY = 1.0;
+float moveSpeed = 1.0;
+bool isGameOver = false;
 
 void init() {
     glClearColor(0.1, 0.6, 0.4, 1.0);
@@ -26,12 +28,41 @@ void drawPlayer() {
     glEnd();
 }
 
+void drawColumn() {
+    glColor3f(0.2, 1.0, 0.3);
+    glBegin(GL_QUADS);
+        glVertex3f(4.2 - moveSpeed, -1.2, -5.0);
+        glVertex3f(6.2 - moveSpeed, -1.2, -5.0);
+        glVertex3f(6.2 - moveSpeed, -5.2, -5.0);
+        glVertex3f(4.2 - moveSpeed, -5.2, -5.0);
+    glEnd();
+}
+
+void gameOver() {
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    isGameOver = true;
+}
+
+void restartGame() {
+    playerPosY = 1.0;
+    moveSpeed = 1.0;
+    isGameOver = false;
+    glClearColor(0.1, 0.6, 0.4, 1.0);
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     glTranslatef(-1, 0, -5);
     drawPlayer();
-    playerPosY -= 0.0005;
+    drawColumn();
+
+    if(4.2 - moveSpeed < 2.2 && 6.2 - moveSpeed > 1.2 && (-2.2) + playerPosY < -1.2) {
+        gameOver();
+    } else {
+        playerPosY -= 0.0005;
+        moveSpeed += 0.0005;
+    }
 }
 
 int main(int argc, char* args[]) {
@@ -43,9 +74,9 @@ int main(int argc, char* args[]) {
         while (SDL_PollEvent(&myevent)) {
             switch(myevent.type) {
                 case SDL_KEYDOWN:
-                    if(myevent.key.keysym.sym == SDLK_SPACE) playerPosY += 0.7;
-                        break;
-
+                    if(myevent.key.keysym.sym == SDLK_SPACE && !isGameOver) playerPosY += 0.7;
+                    if(myevent.key.keysym.sym == SDLK_r) restartGame();
+                    break;
                 case SDL_QUIT:
                     return 0;
                     break;
